f68d873dc160ec7f5b3120f8167998e5a9928576
bundle info <i@e-kzn.ru>
2016-03-28 20:10:49 +0300

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 describe "Статическая страница" do
   describe "Home page" do
     it "должна содердать 'Sample App'" do
@@ -10,6 +10,12 @@ describe "Статическая страница" do
     it "должна содержать 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+  end
+  describe "About page" do
+    it "должна содержать 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
